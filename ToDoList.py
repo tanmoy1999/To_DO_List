ae6c2c47fe989fd7e6cfc3fef0d150edcb0bb3e9
Tanmoy Munshi <42179859+tanmoy1999@users.noreply.github.com>
2020-03-11 19:38:44 +0530

    Basic (1)

---
+++ ToDoList.py
@@ -0,0 +1,13 @@
+workList = []
+n = int(input("Number of work TO_DO"))
+for i in range(0,n):
+    work = input("Work: ")
+    workList.append(work)
+
+for i in range(0,n):
+    print(workList[i])
+
+
+'''k=int(input("enter no to delete: "))
+student.remove(student[k])
+print(student)'''
